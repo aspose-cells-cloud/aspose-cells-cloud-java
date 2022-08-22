9ccd82096305be13609b67ee5138b4c2abc4217c
roy wang <roy.wang@aspose.com>
2022-08-21 21:48:55 -0500

    20220822 update : support rotate row and column.

--- src/main/java/com/aspose/cloud/cells/api/LightCellsApi.java
+++ src/main/java/com/aspose/cloud/cells/api/LightCellsApi.java
@@ -2425,5 +2425,152 @@ public class LightCellsApi {
         apiClient.executeAsync(call, localVarReturnType, callback);
         return call;
     }
+    /**
+     * Build call for postRotate
+     * @param file File to upload (required)
+     * @param rotateType  (required)
+     * @param format  (required)
+     * @param progressListener Progress listener
+     * @param progressRequestListener Progress request listener
+     * @return Call to execute
+     * @throws ApiException If fail to serialize the request body object
+     */
+    public com.squareup.okhttp.Call postRotateCall(HashMap<String,File> file,  String rotateType, String format, Boolean checkExcelRestriction, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        Object localVarPostBody = null;
+        
+        // create path and map variables
+        String localVarPath = "/cells/rotate";
+
+        List<Pair> localVarQueryParams = new ArrayList<Pair>();
+        if (rotateType != null)
+            localVarQueryParams.addAll(apiClient.parameterToPairs("", "rotateType", rotateType));
+        if (format != null)
+            localVarQueryParams.addAll(apiClient.parameterToPairs("", "format", format));  
+        if (checkExcelRestriction != null)
+            localVarQueryParams.addAll(apiClient.parameterToPairs("", "checkExcelRestriction", checkExcelRestriction));
+
+        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+        if (file != null){
+            for (String key : file.keySet()) {
+                localVarFormParams.put(key,file.get(key));                
+            }
+        }
+        final String[] localVarAccepts = {
+            "application/json"
+        };
+        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+
+        final String[] localVarContentTypes = {
+            "multipart/form-data"
+        };
+        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+        localVarHeaderParams.put("Content-Type", localVarContentType);
+
+        if(progressListener != null) {
+            apiClient.getHttpClient().networkInterceptors().add(new com.squareup.okhttp.Interceptor() {
+                @Override
+                public com.squareup.okhttp.Response intercept(com.squareup.okhttp.Interceptor.Chain chain) throws IOException {
+                    com.squareup.okhttp.Response originalResponse = chain.proceed(chain.request());
+                    return originalResponse.newBuilder()
+                    .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                    .build();
+                }
+            });
+        }
+
+        String[] localVarAuthNames = new String[] {  };
+        return apiClient.buildCall(localVarPath, "POST", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames, progressRequestListener);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private com.squareup.okhttp.Call postRotateValidateBeforeCall(HashMap<String,File> file,   String rotateType, String format, Boolean checkExcelRestriction, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        
+        // verify the required parameter 'file' is set
+        if (file == null) {
+            throw new ApiException("Missing the required parameter 'file' when calling postRotate(Async)");
+        }
+        
+        // verify the required parameter 'datasource' is set
+        if (rotateType == null) {
+            throw new ApiException("Missing the required parameter 'text' when calling postRotate(Async)");
+        }
+        // verify the required parameter 'datasource' is set
+        if (format == null) {
+            throw new ApiException("Missing the required parameter 'format' when calling postRotate(Async)");
+        }        
+        
+        com.squareup.okhttp.Call call = postRotateCall(file, rotateType,format,checkExcelRestriction, progressListener, progressRequestListener);
+        return call;
+        
+    }
+
+    /**
+     * 
+     * 
+     * @param file File to upload (required)
+     * @param rotateType  (required)
+     * @param format  (required)
+     * @return FilesResult
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public FilesResult postRotate(HashMap<String,File> file,   String rotateType, String format , Boolean checkExcelRestriction) throws ApiException {
+        ApiResponse<FilesResult> resp = postRotateWithHttpInfo(file,rotateType,format,checkExcelRestriction);
+        return resp.getData();
+    }
+
+    /**
+     * 
+     * 
+     * @param file File to upload (required)
+     * @param rotateType  (required)
+     * @param format  (required)
+     * @return ApiResponse&lt;FilesResult&gt;
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public ApiResponse<FilesResult> postRotateWithHttpInfo(HashMap<String,File> file,  String rotateType, String format, Boolean checkExcelRestriction) throws ApiException {
+        com.squareup.okhttp.Call call = postRotateValidateBeforeCall(file, rotateType,format,checkExcelRestriction, null, null);
+        Type localVarReturnType = new TypeToken<FilesResult>(){}.getType();
+        return apiClient.execute(call, localVarReturnType);
+    }
+
+    /**
+     *  (asynchronously)
+     * 
+     * @param file File to upload (required)
+     * @param rotateType  (required)
+     * @param format  (required)
+     * @param callback The callback to be executed when the API call finishes
+     * @return The request call
+     * @throws ApiException If fail to process the API call, e.g. serializing the request body object
+     */
+    public com.squareup.okhttp.Call postRotateeAsync(HashMap<String,File> file, String rotateType, String format, Boolean checkExcelRestriction, final ApiCallback<FilesResult> callback) throws ApiException {
+
+        ProgressResponseBody.ProgressListener progressListener = null;
+        ProgressRequestBody.ProgressRequestListener progressRequestListener = null;
+
+        if (callback != null) {
+            progressListener = new ProgressResponseBody.ProgressListener() {
+                @Override
+                public void update(long bytesRead, long contentLength, boolean done) {
+                    callback.onDownloadProgress(bytesRead, contentLength, done);
+                }
+            };
+
+            progressRequestListener = new ProgressRequestBody.ProgressRequestListener() {
+                @Override
+                public void onRequestProgress(long bytesWritten, long contentLength, boolean done) {
+                    callback.onUploadProgress(bytesWritten, contentLength, done);
+                }
+            };
+        }
+
+        com.squareup.okhttp.Call call = postRotateValidateBeforeCall(file, rotateType,format,checkExcelRestriction, progressListener, progressRequestListener);
+        Type localVarReturnType = new TypeToken<FilesResult>(){}.getType();
+        apiClient.executeAsync(call, localVarReturnType, callback);
+        return call;
+    }
 
 }
