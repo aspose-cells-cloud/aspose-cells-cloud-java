7275ac89f723e52354335270abb0a2c4bafdfe53
roy.wang <roy.wang@aspose.com>
2023-02-16 09:50:44 +0800

    20230216 update : update CellsError.

--- src/main/java/com/aspose/cloud/cells/model/CellsError.java
+++ src/main/java/com/aspose/cloud/cells/model/CellsError.java
@@ -25,21 +25,14 @@
 package com.aspose.cloud.cells.model;
 
 import java.util.Objects;
-import com.aspose.cloud.cells.model.ErrorDetails;
-import com.google.gson.TypeAdapter;
-import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import java.io.IOException;
 
 /**
  * Error
  */
 @ApiModel(description = "Error")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2022-09-16T03:09:53.733-05:00")
 public class CellsError {
   @SerializedName("Code")
   private String code = null;
