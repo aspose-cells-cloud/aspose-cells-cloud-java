9ef1a35c00bb163a6c50e7fd581a679ea7561e4f
roy.wang <roy.wang@aspose.com>
2023-07-24 19:47:15 -0700

    Add repair api.

--- src/main/java/com/aspose/cloud/cells/api/CellsApi.java
+++ src/main/java/com/aspose/cloud/cells/api/CellsApi.java
@@ -55748,6 +55748,60 @@ public class CellsApi {
 
 
     @SuppressWarnings("rawtypes")
+    private com.squareup.okhttp.Call postRepairValidateBeforeCall(PostRepairRequest request, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException, IOException {
+        return request.buildHttpRequest(apiClient, progressListener, progressRequestListener, true);
+    }
+
+    public  FilesResult  postRepair(PostRepairRequest request) throws ApiException,  IOException {
+        try {
+            ApiResponse< FilesResult > resp = postRepairWithHttpInfo(request);
+             return resp.getData(); 
+        }
+        catch (ApiException ex) {
+            if (ex.getCode() == apiClient.getNotAuthCode()) {
+                apiClient.requestToken();
+                ApiResponse< FilesResult > resp = postRepairWithHttpInfo(request);
+                 return resp.getData();       
+            }
+            throw ex;
+        }
+    }
+
+    private ApiResponse< FilesResult > postRepairWithHttpInfo(PostRepairRequest request) throws ApiException,  IOException {
+        com.squareup.okhttp.Call call =  postRepairValidateBeforeCall(request, null, null);
+        Type localVarReturnType = new TypeToken< FilesResult >(){}.getType();
+        return apiClient.execute(call, localVarReturnType);
+    }
+
+    public com.squareup.okhttp.Call  postRepairAsync( PostRepairRequest request, final ApiCallback< FilesResult > callback) throws ApiException,  IOException {
+
+        ProgressResponseBody.ProgressListener progressListener = null;
+        ProgressRequestBody.ProgressRequestListener progressRequestListener = null;
+
+        if (callback != null) {
+            progressListener = new ProgressResponseBody.ProgressListener() {
+                @Override
+                public void update(long bytesRead, long contentLength, boolean done) {
+                    callback.onDownloadProgress(bytesRead, contentLength, done);
+                }
+            };
+
+            progressRequestListener = new ProgressRequestBody.ProgressRequestListener() {
+                @Override
+                public void onRequestProgress(long bytesWritten, long contentLength, boolean done) {
+                    callback.onUploadProgress(bytesWritten, contentLength, done);
+                }
+            };
+        }
+
+        com.squareup.okhttp.Call call =  postRepairValidateBeforeCall(request, progressListener, progressRequestListener);
+        Type localVarReturnType = new TypeToken< FilesResult >(){}.getType();
+        apiClient.executeAsync(call, localVarReturnType, callback);
+        return call;
+    }
+
+
+    @SuppressWarnings("rawtypes")
     private com.squareup.okhttp.Call postRotateValidateBeforeCall(PostRotateRequest request, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException, IOException {
         return request.buildHttpRequest(apiClient, progressListener, progressRequestListener, true);
     }
---
+++ src/main/java/com/aspose/cloud/cells/request/PostRepairRequest.java
@@ -0,0 +1,128 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="PostRepairRequest.java">
+ *   Copyright (c) 2023 Aspose.Cells Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+package com.aspose.cloud.cells.request;
+import com.aspose.cloud.cells.client.*;
+import com.aspose.cloud.cells.model.*;
+import com.squareup.okhttp.*;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class PostRepairRequest  implements IRequestModel {
+    private HashMap<String,String> extendQueryParameterMap;
+    public HashMap<String,String> getExtendQueryParameterMap() {
+        return this.extendQueryParameterMap;
+    }
+
+    public void setExtendQueryParameterMap( HashMap<String,String>  extendQueryParameterMap) {
+        this.extendQueryParameterMap = extendQueryParameterMap;
+    }
+    private HashMap<String,File> file;
+    private String format;
+        public PostRepairRequest()
+        {
+
+        }
+        public PostRepairRequest(HashMap<String,File> file, String format) {
+            this.file = file;
+            this.format = format;
+        }   
+
+        public HashMap<String,File> getFile() {
+            return file;
+        }
+
+        public void setFile(HashMap<String,File> file) {
+            this.file = file;
+        }
+
+
+        public String getFormat() {
+            return format;
+        }
+
+        public void setFormat(String format) {
+            this.format = format;
+        }
+
+    @Override
+    public Call buildHttpRequest(ApiClient apiClient, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener, Boolean addAuthHeaders) throws ApiException {
+         if (getFile() == null) {
+                    throw new ApiException("Missing the required parameter 'File' when calling PostRepair");
+                }       
+        String localVarPath = "/cells/repair";
+        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+        List<Pair> localVarQueryParams = new ArrayList<Pair>();
+            if (getFormat() != null){
+                localVarQueryParams.addAll(apiClient.parameterToPairs("", "format", getFormat()));
+            }  
+            if(this.extendQueryParameterMap !=null){
+                for (String key :this.extendQueryParameterMap.keySet()) {
+                        localVarQueryParams.addAll(apiClient.parameterToPairs("", key, this.extendQueryParameterMap.get(key)));           
+                }
+            }
+                   
+                if (getFile() != null){
+                    for (String key : getFile().keySet()) {
+                        localVarFormParams.put(key,getFile().get(key));                
+                    }
+                }      
+        Object localVarPostBody = null;
+                final String[] localVarAccepts = {
+                    "application/json"
+                };
+                final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+                if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+
+                final String[] localVarContentTypes = { "application/json" };
+                if(getFile() != null){
+                   localVarContentTypes[0] =  "multipart/form-data";
+                }
+                final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+                localVarHeaderParams.put("Content-Type", localVarContentType);
+
+                if(progressListener != null) {
+                    apiClient.getHttpClient().networkInterceptors().add(new com.squareup.okhttp.Interceptor() {
+                        @Override
+                        public com.squareup.okhttp.Response intercept(com.squareup.okhttp.Interceptor.Chain chain) throws IOException {
+                            com.squareup.okhttp.Response originalResponse = chain.proceed(chain.request());
+                            return originalResponse.newBuilder()
+                            .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                            .build();
+                        }
+                    });
+                }
+
+                String[] localVarAuthNames = new String[] {  };
+                return apiClient.buildCall(localVarPath, "POST", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames, progressRequestListener);
+
+    }
+
+
+}
+
